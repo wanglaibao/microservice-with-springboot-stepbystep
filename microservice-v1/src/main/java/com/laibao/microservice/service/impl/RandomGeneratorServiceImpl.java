7e68abaf6290a527814c77b2988d63f8a2d5b3e3
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-27 13:37:29 +0800

    update java files info

---
+++ microservice-v1/src/main/java/com/laibao/microservice/service/impl/RandomGeneratorServiceImpl.java
@@ -0,0 +1,18 @@
+package com.laibao.microservice.service.impl;
+
+import com.laibao.microservice.service.RandomGeneratorService;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+
+public class RandomGeneratorServiceImpl implements RandomGeneratorService {
+
+    final static int MINIMUM_FACTOR = 11;
+
+    final static int MAXIMUM_FACTOR = 99;
+
+    @Override
+    public int generateRandomFactor() {
+        return new SecureRandom().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
+    }
+}
