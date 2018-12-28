9b6fe90cb5e2f2c3debad374f969d962f0dd4f00
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-12-28 16:33:54 +0800

    update pom.xml file info

---
+++ gamification/src/main/java/com/laibao/microservice/domain/Badge.java
@@ -0,0 +1,15 @@
+package com.laibao.microservice.domain;
+
+/**
+ * Enumeration with the different types of Badges that a user can win.
+ */
+public enum Badge {
+    // Badges depending on score
+    BRONZE_MULTIPLICATOR,
+    SILVER_MULTIPLICATOR,
+    GOLD_MULTIPLICATOR,
+
+    // Other badges won for different conditions
+    FIRST_ATTEMPT,
+    FIRST_WON
+}
